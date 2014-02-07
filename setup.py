660c35cb6735d6f79fb3fd190b217d0b515718ed
Christian Fobel <christian@fobel.net>
2014-02-07 08:19:47 -0500

    Restructure as Python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='svg_model',
+      version='0.0.1',
+      description='A Python module for parsing an SVG file to a group of '
+      'paths.',
+      keywords='svg model pymunk',
+      author='Christian Fobel',
+      url='http://github.com/cfobel/svg_model.git',
+      license='GPL',
+      packages=['svg_model', 'svg_model.svgload'])
