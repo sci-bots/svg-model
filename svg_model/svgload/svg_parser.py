218d992f2252e4541acbab229aab5a4da82dc9ff
Christian Fobel <christian@fobel.net>
2014-03-18 10:40:13 -0400

    Replace `path` package with `path_helpers`

--- svg_model/svgload/svg_parser.py
+++ svg_model/svgload/svg_parser.py
@@ -17,7 +17,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 import warnings
 
 from lxml import etree
-from path import path
+from path_helpers import path
 
 from .path_parser import PathParser, ParseError
 from ..loop import Loop
