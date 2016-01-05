931d0dc4596b755cafab88bf9f369adea46dc7da
Christian Fobel <christian@fobel.net>
2016-01-05 12:19:04 -0500

    Use pavement.py instead of setup.py

--- pavement.py
+++ pavement.py
@@ -1,7 +1,11 @@
 import sys
-from distutils.core import setup
 
-sys.path.insert(0, '.')
+from paver.easy import task, needs, path, sh, cmdopts, options
+from paver.setuputils import setup, install_distutils_tasks
+from distutils.extension import Extension
+from distutils.dep_util import newer
+
+sys.path.insert(0, path('.').abspath())
 import version
 
 
@@ -13,8 +17,15 @@ setup(name='svg_model',
       author='Christian Fobel',
       author_email='christian@fobel.net',
       url='http://github.com/wheeler-microfluidics/svg_model.git',
-      license='GPL',
+      license='LGPLv2.1',
       install_requires=['numpy', 'pandas', 'pint', 'pymunk>=4.0.0'],
       packages=['svg_model'],
       # Install data listed in `MANIFEST.in`
       include_package_data=True)
+
+
+@task
+@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
+def sdist():
+    """Overrides sdist to make sure that our setup.py is generated."""
+    pass
