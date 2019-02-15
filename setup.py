fc6056a80c27c76ad35d06107233db3a69ede159
Christian Fobel <christian@fobel.net>
2019-02-15 00:15:16 -0500

    build(setup): simplify to not use Paver
    Use `setup.py` rather than `pavement.py` to reduce complexity.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 from __future__ import absolute_import
-from __future__ import unicode_literals
-import sys
+from __future__ import print_function
 
-from paver.easy import task, needs, path, sh, cmdopts, options
-from paver.setuputils import setup, install_distutils_tasks
-from distutils.extension import Extension
-from distutils.dep_util import newer
+from glob import glob
+from os.path import basename
+from os.path import splitext
+from setuptools import find_packages
+from setuptools import setup
 
 sys.path.insert(0, path('.').abspath())
 import version
 
-
+# See https://blog.ionelmc.ro/2014/06/25/python-packaging-pitfalls/
 setup(name='svg_model',
       version=version.getVersion(),
       description='A Python module for parsing an SVG file to a group of '
@@ -18,17 +20,10 @@ setup(name='svg_model',
       keywords='svg model pymunk',
       author='Christian Fobel',
       author_email='christian@fobel.net',
-      url='http://github.com/wheeler-microfluidics/svg_model.git',
+      url='https://github.com/sci-bots/svg-model',
       license='LGPLv2.1',
       install_requires=['lxml', 'pandas>=0.17.0', 'path-helpers', 'pint',
                         'pymunk>=4.0.0,<5.0', 'svgwrite'],
       packages=['svg_model'],
       # Install data listed in `MANIFEST.in`
       include_package_data=True)
-
-
-@task
-@needs('generate_setup', 'minilib', 'setuptools.command.sdist')
-def sdist():
-    """Overrides sdist to make sure that our setup.py is generated."""
-    pass
